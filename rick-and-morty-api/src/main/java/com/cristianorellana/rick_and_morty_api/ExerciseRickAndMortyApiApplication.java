7aeccf62193d1beac46ff4961b3c2389fe53ca3f
CristianOrellanna <kristian13orellanagalaz@gmail.com>
2024-08-12 17:09:54 -0700

    Project start

---
+++ rick-and-morty-api/src/main/java/com/cristianorellana/rick_and_morty_api/ExerciseRickAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.cristianorellana.rick_and_morty_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseRickAndMortyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseRickAndMortyApiApplication.class, args);
+	}
+
+}
---
+++ rick-and-morty-api/src/test/java/com/cristianorellana/rick_and_morty_api/ExerciseRickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristianorellana.rick_and_morty_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseRickAndMortyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
