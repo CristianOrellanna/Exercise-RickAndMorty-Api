f0cf39d0ab2ef51ed023ab986abcff5ab6a74b4b
krist <kristian13orellanagalaz@gmail.com>
2024-10-01 15:11:06 -0300

    change: "URLs to application.properties"

--- rick-and-morty-api/src/main/java/com/cristianorellana/rick_and_morty_api/Service/RickAndMortyService.java
+++ rick-and-morty-api/src/main/java/com/cristianorellana/rick_and_morty_api/Service/RickAndMortyService.java
@@ -3,6 +3,7 @@ package com.cristianorellana.rick_and_morty_api.Service;
 import com.cristianorellana.rick_and_morty_api.Model.Character;
 import com.cristianorellana.rick_and_morty_api.Model.Origin;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -12,13 +13,19 @@ public class RickAndMortyService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Value("${rickAndMorty.api.character}")
+    private String apiCharacterUrl;
+
+    @Value("${rickAndMorty.api.location}")
+    private String apiLocationUrl;
+
     public Character getCharacterById(int id) {
-        String url = "https://rickandmortyapi.com/api/character/" + id;
+        String url = apiCharacterUrl + id;
         return restTemplate.getForObject(url, Character.class);
     }
 
     public Origin getLocationById(int id) {
-        String url = "https://rickandmortyapi.com/api/location/" + id;
+        String url = apiLocationUrl + id;
         return restTemplate.getForObject(url, Origin.class);
     }
 }
